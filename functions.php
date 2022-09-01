7368747c4e64cc92477b57cbd5a34955bb897cfd
Elizeu <elizeu_os@hotmail.com>
2022-09-01 17:41:49 -0300

    Add script and style

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+//Add css
+wp_enqueue_style( 'Fancybox For Wordpress', '/fancybox/jquery.fancybox-1.3.4.css' )
+
+//Add js
+wp_enqueue_script( 'Fancybox For Wordpress', '/fancybox/jquery.fancybox-1.3.4.js', true );
