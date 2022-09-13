d8351cfdf9c53015343927282b2faf55798044c4
Elizeu <elizeu_os@hotmail.com>
2022-09-13 12:44:05 -0300

    Change version to 1.0.0

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* Plugin Name: Fancybox For Wordpress
+* Description: Just a compilation of Fancybox jquery plugin: http://fancybox.net/
+* Version: 1.0.0
+* Author: Elizeu Oliveira da Silva
+**/
